6cd9bcc2e5ef74f3f27cc49e6667ea892b865603
sy430 <sy4306122@gmail.com>
2021-01-03 22:10:16 +0530

    Initial commit
    Signed-off-by: AMIT YADAV <sy4306122@users.noreply.github.com>
    Signed-off-by: sy430 <sy4306122@gmail.com>

---
+++ app/src/main/java/com/example/apnadarzi/Profile_Update.java
@@ -0,0 +1,233 @@
+package com.example.apnadarzi;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.apnadarzi.Prevalent.Prevalent;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class Profile_Update extends AppCompatActivity {
+    private static final int GalleryPick = 1;
+    private CircleImageView profileImageView;
+    private EditText fullNameEditText, userPhoneEditText, addressEditText, cityEditText, stateEditText;
+    private TextView profileChangeTextBtn, closeTextBtn, saveTextButton;
+    private Uri imageUri;
+    private String myUrl = "";
+    private StorageTask uploadTask;
+    private StorageReference storageProfilePrictureRef;
+    private String checker = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_update);
+        storageProfilePrictureRef = FirebaseStorage.getInstance().getReference().child("Profile pictures");
+
+        profileImageView = findViewById(R.id.settings_profile_image);
+        fullNameEditText = findViewById(R.id.settings_full_name);
+        userPhoneEditText = findViewById(R.id.settings_phone_number);
+        addressEditText = findViewById(R.id.settings_streetname);
+        cityEditText = findViewById(R.id.settings_city);
+        stateEditText = findViewById(R.id.settings_State);
+        profileChangeTextBtn = findViewById(R.id.profile_image_change_btn);
+        closeTextBtn = findViewById(R.id.close_settings_btn);
+        saveTextButton = findViewById(R.id.update_account_settings_btn);
+
+        userInfoDisplay(profileImageView, fullNameEditText, userPhoneEditText, addressEditText);
+
+        closeTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        saveTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checker.equals("clicked")) {
+                    userInfoSaved();
+                } else {
+                    updateOnlyUserInfo();
+                }
+            }
+        });
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checker = "clicked";
+                OpenGallery();
+            }
+        });
+
+        /*profileChangeTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                checker = "clicked";
+                OpenGallery();
+                //CropImage.activity(imageUri).setAspectRatio(1, 1).start(SettinsActivity.this);
+            }
+        });
+
+         */
+    }
+
+    private void updateOnlyUserInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        HashMap<String, Object> userMap = new HashMap<>();
+        userMap.put("name", fullNameEditText.getText().toString());
+        userMap.put("address", addressEditText.getText().toString());
+        userMap.put("city", cityEditText.getText().toString());
+        userMap.put("state", stateEditText.getText().toString());
+        userMap.put("phoneOrder", userPhoneEditText.getText().toString());
+        ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
+        startActivity(new Intent(Profile_Update.this, MainActivity.class));
+        Toast.makeText(Profile_Update.this, "Profile Info update successfully.", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GalleryPick && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            profileImageView.setImageURI(imageUri);
+        } else {
+
+            Toast.makeText(this, "Error, Try Again.", Toast.LENGTH_SHORT).show();
+
+            startActivity(new Intent(Profile_Update.this, Profile_Update.class));
+            finish();
+        }
+    }
+
+
+    private void userInfoSaved() {
+        if (TextUtils.isEmpty(fullNameEditText.getText().toString())) {
+            Toast.makeText(this, "Name is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(addressEditText.getText().toString())) {
+            Toast.makeText(this, "Address is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(userPhoneEditText.getText().toString())) {
+            Toast.makeText(this, "Phone is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(cityEditText.getText().toString())) {
+            Toast.makeText(this, "City is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(cityEditText.getText().toString())) {
+            Toast.makeText(this, "State is mandatory.", Toast.LENGTH_SHORT).show();
+        } else if (checker.equals("clicked")) {
+            uploadImage();
+        }
+    }
+
+    private void uploadImage() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Update Profile");
+        progressDialog.setMessage("Please wait, while we are updating your account information");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        if (imageUri != null) {
+            final StorageReference fileRef = storageProfilePrictureRef
+                    .child(Prevalent.currentOnlineUser.getPhone() + ".jpg");
+
+            uploadTask = fileRef.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUrl = task.getResult();
+                        myUrl = downloadUrl.toString();
+                        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Users");
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("name", fullNameEditText.getText().toString());
+                        userMap.put("address", addressEditText.getText().toString());
+                        userMap.put("phoneOrder", userPhoneEditText.getText().toString());
+                        userMap.put("image", myUrl);
+                        ref.child(Prevalent.currentOnlineUser.getPhone()).updateChildren(userMap);
+                        progressDialog.dismiss();
+                        startActivity(new Intent(Profile_Update.this, MainActivity.class));
+                        Toast.makeText(Profile_Update.this, "Profile Info update successfully.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(Profile_Update.this, "Error.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "image is not selected.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void userInfoDisplay(final CircleImageView profileImageView, final EditText fullNameEditText, final EditText userPhoneEditText, final EditText addressEditText) {
+        DatabaseReference UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(Prevalent.currentOnlineUser.getPhone());
+        UsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    if (dataSnapshot.child("image").exists()) {
+                        String image = dataSnapshot.child("image").getValue().toString();
+                        String name = dataSnapshot.child("name").getValue().toString();
+                        String phone = dataSnapshot.child("phone").getValue().toString();
+                        String address = dataSnapshot.child("address").getValue().toString();
+                        Picasso.get().load(image).placeholder(R.drawable.profile).into(profileImageView);
+                        fullNameEditText.setText(name);
+                        userPhoneEditText.setText(phone);
+                        addressEditText.setText(address);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+}
